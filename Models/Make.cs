5b1b77c66a9d2962ce5fd340e94f4cfe90e22040
Unknown <petinari@outlook.com>
2018-04-28 17:57:48 -0400

    k

--- Models/Make.cs
+++
@@ -1,9 +0,0 @@
-namespace cursoAspNetCore.Models
-{
-    public class Make
-    {
-        public int Id { get; set; } 
-        public string Name { get; set; }
-        public ICollec MyProperty { get; set; }
-    }
-}
--- Models/Model.cs
+++
@@ -1,8 +0,0 @@
-namespace cursoAspNetCore.Models
-{
-    public class Model
-    {
-        public int id { get; set; } 
-        public string Name { get; set; }    
-    }
-}

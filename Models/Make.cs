6199295363466b34a6785c8a4b9170d82814e0d5
Unknown <petinari@outlook.com>
2018-04-28 17:29:32 -0400

    w

---
+++ Models/Make.cs
@@ -0,0 +1,9 @@
+namespace cursoAspNetCore.Models
+{
+    public class Make
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public ICollec MyProperty { get; set; }
+    }
+}
---
+++ Models/Model.cs
@@ -0,0 +1,8 @@
+namespace cursoAspNetCore.Models
+{
+    public class Model
+    {
+        public int id { get; set; } 
+        public string Name { get; set; }    
+    }
+}
